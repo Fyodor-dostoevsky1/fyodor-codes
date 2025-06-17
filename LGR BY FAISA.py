bc8441761164b960537752cad0cda922c1d160f6
Fyodor-dostoevsky1 <faisalff620413596912345@gmail.com>
2025-06-17 22:39:56 +0530

    Create LGR BY FAISA.py

---
+++ LGR BY FAISA.py
@@ -0,0 +1,129 @@
+a   =   input('enter your name:')
+print(a)
+print('Hello',a,'Nice to meet you')
+b  =  input('Are you Sure you want to play''Lets get Rich:''')
+print(b)
+if (b == 'YES'):
+    print('Thats Good Lets play LGR')
+else:
+    print('NO PROBLEM COMBACK WHEN EVER YOU ARE READY') 
+    exit()
+
+print('''SO,we are expection that you already knows the rules
+      LETS  PLAY LGR''')
+
+questions   =   ['1. What is the capital of France?','2. Which planet is known as the "Red Planet"?','3. What is the chemical symbol for water?', '4.Who painted the Mona Lisa','5. What is the area of a triangle with base 12 cm and height 8 cm?' ]
+options =   ['A.Newyork\nB.Delhi\nC.Madrid\nD.Taiwan','A) Venus\nB) Mars\nC) Jupiter\nD) Saturn','A) H₂O\nB) CO₂\nC) NaCl\nD) O₂\n','A.Leonardo da Vinci\nB.Michelangelo\nC.Raphael\nD.Sandro Botticelli','a) 20 cm²\nb) 48 cm²\nc) 96 cm²\nd) 40 cm²']
+print(questions[0])
+print(options[0])
+c=  input('Please Enter Your Answer:')
+if  c=='A':
+    print('Yes You are Right\nYou have just won 10000000 UNITS!!!\nCONGRATULATIONS')
+else:
+    print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nLoser GET THROWED OUT SO SOON')
+    exit()
+    
+print('The  second  Question for 20000000 UNITS is:')
+print(questions[1])
+print(options[1])
+d=  input('Please Enter Your Answer:')
+if  d== 'B':
+     print('Yes You are Right\nYou have just won 20000000 UNITS!!!\nCONGRATULATIONS')
+else:
+     print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nLoser GET THROWED OUT SO SOON')
+     exit()
+print('The Third Question for 30000000 UNITS is:')
+print(questions[2])
+print(options[2])
+e=  input('Please Enter Your Answer:')
+if  e== 'A':
+     print('Yes You are Right\nYou have just won 30000000 UNITS!!!\nCONGRATULATIONS')
+else:
+     print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nLoser GET THROWED OUT SO SOON')
+     exit()
+print('Your Fourth Question For 4000000 is:')
+print(questions[3])
+print(options[3])
+f=input('Please Enter Your Answer:')
+if  f== 'A':
+  print('Yes You are Right\nYou have just won 40000000 UNITS!!!\nCONGRATULATIONS')
+else:
+ print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nLoser GET THROWED OUT SO SOON')
+ exit()
+print('Your Fifth questions For 50000000 is:')
+print(questions[4])
+print(options[4])
+g = input('Please Enter Your Answer')
+if g =='B':
+    print('Yes You are Right\nYou have just won 1000000 UNITS!!!\nCONGRATULATIONS')
+else:
+    print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nLoser GET THROWED OUT SO SOON')
+    exit()
+
+print('Nice you Have Cleared the 1st Phase ', )
+print('The Final Phase begins')
+
+questions2  =   ['6. The Great Wall of China was built primarily to protect against invasions from which direction', '7.Which empire was ruled by Julius Caesar?','8.What is the smallest country in the world?','9.Which spice is derived from the Crocus flower?','10.Who wrote The book called ''DEMONS'' which is in classic literarture genre']
+options2 =   ['''a) South\nb) East
+c) West
+d) North''','''a) Greek Empire
+b) Roman Empire
+c) Persian Empire
+d) Ottoman Empire''','''a) Monaco
+b) San Marino
+c) Vatican City
+d) Liechtenstein''','''a) Cinnamon
+b) Cardamom
+c) Saffron
+d) Turmeric''',  'A.Franz Kafka\nB.Oscar Wilde\nC.Albert Camus\nD.Fyodor Mikhailovich Dostoevsky']
+print('Your sixth question is for 60000000 units:')
+print(questions2[0])
+print(options2[0])
+
+h=input('Please Enter Your Answer:')
+if  h== 'D':
+    print('Yes You are Right\nYou have just won 6000000 UNITS!!!\nCONGRATULATIONS')
+else:
+    print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nYOU PLAYED SO WELL \nbetter luck next time')
+    exit()
+
+print('Your seventh question is for 70000000 units:')
+print(questions2[1])
+print(options2[1])
+j=  input('Please Enter your Answer:')
+if  j=='B':
+    print('Yes You are Right\nYou have just won 70000000 UNITS!!!\nCONGRATULATIONS')
+else:
+    print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nYOU PLAYED SO WELL \nbetter luck next time')
+    exit()
+print('Your Eighth question is for 80000000 units:')
+print(questions2[2])
+print(options2[2])
+
+k=input('Please Enter your Answeer:')
+if  k== 'C':
+    print('Yes You are Right\nYou have just won 80000000 UNITS!!!\nCONGRATULATIONS')
+else:
+    print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nYOU PLAYED SO WELL \nbetter luck next time')
+    exit()
+print('Your Ninth question for 90000000 units is:')
+print(questions2[3])
+print(options2[3])
+l  =    input('Please Enter Your Answer:')
+if l=='C':
+    print('Yes You are Right\nYou have just won 90000000 UNITS!!!\nCONGRATULATIONS')
+else:
+    print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nYOU WERE SO CLOSE FOR JACKPOT\nBETTER LUCK NEXT TIME')
+    exit()
+print('YOU HAVE BEEN IN THE GAME AND PLAYED SO WELL\nNOW LAST QUESTIONS FOR THE JACKPOT IS LEFT\nCONGRATULATIONS ')
+print('Your Tenth and Final Question for JACKPOT :) is: ')
+print(questions2[4])
+print(options2[4])
+m  =    input('Please Enter Your Answer:')
+if  m=='D':
+    print('Yes You are Right\nYou have just won 100000000 UNITS!!!\nCONGRATULATIONS')
+else:
+    print('YOU ARE WRONG\nSORRY,YOU ARE OUT OF KBC\nYOU WERE SO CLOSE FOR JACKPOT\nBETTER LUCK NEXT TIME')
+    exit()
+print('A huge congratulations to you \nYOU COMPLETED THE WHO WILL GET RICH FIRST\nThe prize pool will be credited Soon\nIt was nice playing You..')
+print('THANK YOU ')

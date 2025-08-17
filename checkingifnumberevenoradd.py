31dcbcae3950dd387e6ef16b408f456cee0feeda
Fyodor-dostoevsky1 <faisalff620413596912345@gmail.com>
2025-08-17 20:08:41 +0530

    Add files via upload

---
+++ checkingifnumberevenoradd.py
@@ -0,0 +1,17 @@
+
+def even(a):
+    if int(a % 2 ==  0 ):
+      print('The Number is Even')
+    else:
+       return a
+def odd(a):
+  if int(a %2 != 0):
+    print('The number is odd ')
+  else: 
+     return a
+  
+a = int(input('Please enter your Number to Check :'))
+even(a)
+odd(a)
+
+ 
